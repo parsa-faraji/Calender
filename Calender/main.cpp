b16bb354d45f55a42da513d90ba34a15e4e7646e
parsa-faraji <parsafaraji8213@gmail.com>
2024-12-21 11:32:03 -0800

    Initial Commit

---
+++ Calender/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calender
+//
+//  Created by Parsa Faraji on 12/21/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
